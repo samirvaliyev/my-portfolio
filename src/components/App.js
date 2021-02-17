9079fb61f5729d4c3fda82031e0af30182009425
Samir Valiyev <samir.valiyev@gmail.com>
2021-02-17 12:17:46 +0400

    added search input functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MovieList from "./MovieList";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 	state = {
@@ -33,25 +34,69 @@ class App extends React.Component {
 				imageURL:
 					"https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
 			},
+			{
+				id: 4,
+				name: "Rogue",
+				rating: 7.4,
+				overview:
+					"This is a wider card with supporting text below as a natural lead-in to additional content.",
+				imageURL:
+					"https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+			},
+
+			{
+				id: 5,
+				name: "Project Power",
+				rating: 6.7,
+				overview:
+					"This is a wider card with supporting text below as a natural lead-in to additional content.",
+				imageURL:
+					"https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg",
+			},
+
+			{
+				id: 6,
+				name: "Superman",
+				rating: 7.6,
+				overview:
+					"This is a wider card with supporting text below as a natural lead-in to additional content.",
+				imageURL:
+					"https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg",
+			},
 		],
+
+		searchQuery: "",
 	};
 
 	deleteMovie = (movie) => {
 		const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
-		this.setState({
+
+		this.setState((state) => ({
 			movies: newMovieList,
-		});
+		}));
 	};
+
+	searchMovie = (event) => {
+		// console.log(event.target.value);
+		this.setState({ searchQuery: event.target.value });
+	};
+
 	render() {
+		let filteredMovies = this.state.movies.filter((movie) => {
+			return (
+				movie.name
+					.toLowerCase()
+					.indexOf(this.state.searchQuery.toLowerCase()) !== -1
+			);
+		});
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-lg-12"></div>
+					<div className="col-lg-12">
+						<SearchBar searchMovieProp={this.searchMovie} />
+					</div>
 				</div>
-				<MovieList
-					movies={this.state.movies}
-					deleteMovieProp={this.deleteMovie}
-				/>
+				<MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
 			</div>
 		);
 	}
